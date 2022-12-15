69fbe8a844117aeb461c30fa03202ee3108445c7
Joshua Li <joshuazli2020@gmail.com>
2022-12-15 11:27:41 +1000

    adding css, button functionality for leaving and joining and multiple video functionality

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ let localTracks = [];
 let remoteUsers = {};
 
 let joinAndDisplayLocalStream = async () => {
+  client.on("user-published", handleUserPublished);
+  client.on("user-left", handleUserLeft);
+
   let UID = await client.join(APP_ID, CHANNEL, TOKEN, null);
 
   localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
@@ -32,4 +35,50 @@ let joinStream = async () => {
   document.getElementById("stream-controls").style.display = "flex";
 };
 
+let handleUserJoined = async (user, mediaType) => {
+  remoteUsers[user.uid] = user;
+  await client.subscribe(user, mediaType);
+
+  if (mediaType === "video") {
+    let player = document.getElementById(`user-container-${user.uid}`);
+    if (player != null) {
+      player.remove();
+    }
+
+    player = `<div class="video-container" id="user-container-${user.uid}">
+    <div class="video-player" id="user-${UID}"></div>
+    </div>`;
+
+    document
+      .getElementById("video-streams")
+      .insertAdjacentElement("beforeend", player);
+
+    user.videoTrack.play(`user-${user.uid}`);
+  }
+};
+
+if (mediaType === "audio") {
+  user.audioTrack.play();
+}
+
+let handleUserLeft = async (user) => {
+  delete remoteUsers[user.uid];
+  document.getElementById(`user-container-${user.uid}`).remove();
+};
+
+let leaveAndRemoveLocalStream = async () => {
+  for (let i = 0; localTracks.length > i; i++) {
+    localTracks[i].stop();
+    localTracks[i].close();
+  }
+
+  await client.leave();
+  document.getElementById("join-btn").style.display = "block";
+  document.getElementById("stream-controls").style.display = "none";
+  document.getElementById("video-streams").innerHTML = "";
+};
+
 document.getElementById("join-btn").addEventListener("click", joinStream);
+document
+  .getElementById("leave-btn")
+  .addEventListener("click", leaveAndRemoveLocalStream);
