ec50cbb07fbac3314b7170e20a7f1813bc6dfc5c
Joshua Li <joshuazli2020@gmail.com>
2022-12-15 11:13:15 +1000

    adding functionality with agora sdk

--- main.js
+++ main.js
@@ -7,3 +7,29 @@ const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 
 let localTracks = [];
 let remoteUsers = {};
+
+let joinAndDisplayLocalStream = async () => {
+  let UID = await client.join(APP_ID, CHANNEL, TOKEN, null);
+
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+
+  let player = `<div class="video-container" id="user-container-${UID}">
+<div class="video-player" id="user-${UID}"></div>
+</div>`;
+
+  document
+    .getElementById("video-streams")
+    .insertAdjacentElement("beforeend", player);
+
+  localTracks[1].play(`user-${UID}`);
+
+  await client.publish([localTracks[0], localTracks[1]]);
+};
+
+let joinStream = async () => {
+  await joinAndDisplayLocalStream();
+  document.getElementById("join-btn").style.display = "none";
+  document.getElementById("stream-controls").style.display = "flex";
+};
+
+document.getElementById("join-btn").addEventListener("click", joinStream);
